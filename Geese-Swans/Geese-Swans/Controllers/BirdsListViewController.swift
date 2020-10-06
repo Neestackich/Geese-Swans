def218dc09a21e0ff9b97664509f138ba8443e4b
Neestackich <neestackich@gmail.com>
2020-10-06 17:11:56 +0300

    initial commit

--- Geese-Swans/Geese-Swans/Controllers/BirdsListViewController.swift
+++ Geese-Swans/Geese-Swans/Controllers/BirdsListViewController.swift
@@ -5,4 +5,11 @@
 //  Created by Neestackich on 06.10.2020.
 //
 
-import Foundation
+import UIKit
+
+class BirdsListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
