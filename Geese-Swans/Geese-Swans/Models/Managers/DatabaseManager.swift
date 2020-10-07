b865fdda0b2885c4a79341648b8d412077be4b68
Neestackich <neestackich@gmail.com>
2020-10-07 20:23:48 +0300

    initial commit

---
+++ Geese-Swans/Geese-Swans/Models/Managers/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  Geese-Swans
+//
+//  Created by Neestackich on 06.10.2020.
+//
+
+import Foundation
---
+++ Geese-Swans/Geese-Swans/Views/BirdTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BirdTableViewCell.swift
+//  Geese-Swans
+//
+//  Created by Neestackich on 07.10.2020.
+//
+
+import UIKit
+
+class BirdTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
