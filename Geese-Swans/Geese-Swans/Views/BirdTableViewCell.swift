822837dcc147c53e89b5257192ff61375106c496
Neestackich <neestackich@gmail.com>
2020-10-08 21:18:21 +0300

    added fields to cell

--- Geese-Swans/Geese-Swans/Views/BirdTableViewCell.swift
+++ Geese-Swans/Geese-Swans/Views/BirdTableViewCell.swift
@@ -9,8 +9,20 @@ import UIKit
 
 class BirdTableViewCell: UITableViewCell {
 
+    
+    // MARK: -Properties
+    
     @IBOutlet weak var cellMainView: UIView!
     @IBOutlet weak var birdColor: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var xCoordinateLabel: UILabel!
+    @IBOutlet weak var yCoordinateLabel: UILabel!
+    @IBOutlet weak var birdImage: UIImageView!
+    
+    
+    // MARK: -Methods
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,6 +36,27 @@ class BirdTableViewCell: UITableViewCell {
     
     func setup() {
         cellMainView.layer.cornerRadius = 25
-        birdColor.layer.cornerRadius = 40
+        birdImage.layer.cornerRadius = 40
+    }
+    
+    func configure(bird: Bird) {
+        nameLabel.text = bird.name
+        sizeLabel.text = String(bird.size)
+        xCoordinateLabel.text = String(bird.x)
+        yCoordinateLabel.text = String(bird.y)
+        
+        birdImage.tintColor = bird.color
+        
+        if bird.isFlying {
+            statusLabel.text = "Flying now"
+        } else {
+            statusLabel.text = "Not flying"
+        }
+        
+        if bird.type == Figure.square.rawValue {
+            birdImage.image = UIImage(systemName: "square.fill")
+        } else {
+            birdImage.image = UIImage(systemName: "triangle.fill")
+        }
     }
 }
