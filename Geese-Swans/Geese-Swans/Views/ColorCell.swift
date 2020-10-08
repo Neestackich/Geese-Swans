7382f15d1f518f593eda5dbe684ccff9f884c64f
Neestackich <neestackich@gmail.com>
2020-10-08 12:23:17 +0300

    create color cell for color picker

--- Geese-Swans/Geese-Swans/Views/ColorCell.swift
+++ Geese-Swans/Geese-Swans/Views/ColorCell.swift
@@ -5,4 +5,23 @@
 //  Created by Neestackich on 08.10.2020.
 //
 
-import Foundation
+import UIKit
+
+class ColorCell: UITableViewCell {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+       setup()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func setup() {
+       colorView.layer.cornerRadius = 25
+    }
+}
