b42b0ff74e34146d5370549b875d223445d46e06
Neestackich <neestackich@gmail.com>
2020-10-10 23:41:00 +0300

    added movement and animations to views

--- Geese-Swans/Geese-Swans/Controllers/PlaygroundViewController.swift
+++ Geese-Swans/Geese-Swans/Controllers/PlaygroundViewController.swift
@@ -20,7 +20,8 @@ class PlaygroundViewController: UIViewController, ViewControllerDelegate {
     @IBOutlet weak var showBirdsListButton: UIButton!
     
     var birdsList: [Bird] = []
-
+    var birdsOnPlayground: [UIView : Bird] = [:]
+    
     
     // MARK: -Methods
     
@@ -28,10 +29,6 @@ class PlaygroundViewController: UIViewController, ViewControllerDelegate {
         super.viewDidLoad()
         
         setup()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        birdsList = DatabaseManager.shared.getCoreDataBirds()
         addBirdsToPlayground()
     }
     
@@ -46,12 +43,20 @@ class PlaygroundViewController: UIViewController, ViewControllerDelegate {
         
         landView.layer.cornerRadius = 10
         landView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        birdsList = DatabaseManager.shared.getCoreDataBirds()
+        
+        skyView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(skyTapHandler)))
+        landView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(landTapHandler)))
     }
     
     func addBirdsToPlayground() {
         for bird in birdsList {
             if let newBird = BirdsCreator.shared.createBird(type: bird.type, bird: bird) {
+                newBird.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panGestureHandler)))
                 skyView.addSubview(newBird)
+                
+                birdsOnPlayground[newBird] = bird
             }
         }
     }
@@ -62,6 +67,8 @@ class PlaygroundViewController: UIViewController, ViewControllerDelegate {
     @IBAction func birdsListButtonClick(_ sender: Any) {
         let birdsListViewController = storyboard?.instantiateViewController(withIdentifier: "BirdsListViewController") as! BirdsListViewController
         birdsListViewController.modalPresentationStyle = .fullScreen
+        birdsListViewController.delegate = self
+        birdsListViewController.skyView = skyView
         
         present(birdsListViewController, animated: true)
     }
@@ -70,6 +77,7 @@ class PlaygroundViewController: UIViewController, ViewControllerDelegate {
         let addBirdViewController = storyboard?.instantiateViewController(withIdentifier: "AddBirdViewController") as! AddBirdViewController
         addBirdViewController.modalPresentationStyle = .fullScreen
         addBirdViewController.delegate = self
+        addBirdViewController.skyView = skyView
         
         present(addBirdViewController, animated: true)
     }
@@ -81,10 +89,86 @@ class PlaygroundViewController: UIViewController, ViewControllerDelegate {
         }
     }
     
+    @objc func panGestureHandler(gesture: UIPanGestureRecognizer, viewToMove: UIView) {
+        if let isFlying = birdsOnPlayground[gesture.view!]?.isFlying, isFlying == false {
+            if gesture.state == .began {
+                print("began")
+            } else if gesture.state == .changed {
+                let translation = gesture.translation(in: self.skyView)
+                let viewHalfHeight = gesture.view!.bounds.height / 2
+                let viewHalfWidth = gesture.view!.bounds.width / 2
+                
+                if (gesture.view!.center.x + viewHalfWidth + translation.x) < skyView.bounds.maxX
+                    && (gesture.view!.center.x - viewHalfWidth + translation.x) > skyView.bounds.minX {
+                    gesture.view!.center.x = gesture.view!.center.x + translation.x
+                    gesture.setTranslation(CGPoint.zero, in: skyView)
+                }
+                
+                if (gesture.view!.center.y + viewHalfHeight + translation.y) < skyView.bounds.maxY - 16
+                    && (gesture.view!.center.y - viewHalfHeight + translation.y) > skyView.bounds.minY {
+                    gesture.view!.center.y = gesture.view!.center.y + translation.y
+                    gesture.setTranslation(CGPoint.zero, in: skyView)
+                }
+                
+                birdsOnPlayground[gesture.view!]?.x = Float(gesture.view!.center.x - viewHalfWidth)
+                birdsOnPlayground[gesture.view!]?.y = Float(gesture.view!.center.y - viewHalfHeight)
+            } else if gesture.state == .ended {
+                print("ended")
+                birdsOnPlayground[gesture.view!]?.isFlying = true
+                
+                let queue = DispatchQueue.global(qos: .default)
+                queue.async {
+                    let animation = CABasicAnimation(keyPath: "position")
+                    animation.duration = 5.0
+                    
+                    DispatchQueue.main.async {
+                        animation.fromValue = [gesture.view!.center.x, gesture.view!.center.y]
+                        animation.toValue = [(self.skyView.bounds.minX + gesture.view!.bounds.width / 2), gesture.view!.center.y]
+                        gesture.view!.layer.add(animation, forKey: "position")
+                        
+                        gesture.view!.center.x = self.skyView.bounds.minX + gesture.view!.bounds.width / 2
+                        self.birdsOnPlayground[gesture.view!]?.x = Float(self.skyView.bounds.minX)
+                        DatabaseManager.shared.updateCoreDataBird()
+                    }
+                }
+            }
+        }
+        
+        DatabaseManager.shared.updateCoreDataBird()
+    }
+    
+    @objc func skyTapHandler() {
+        
+    }
+    
+    @objc func landTapHandler() {
+        skyView.subviews.forEach {
+            if let isFlying = birdsOnPlayground[$0]?.isFlying, isFlying {
+                let viewHeight = $0.bounds.height
+                let viewWidth = $0.bounds.width
+                let landedCoordinateX = CGFloat.random(in: viewWidth...(skyView.bounds.width - viewWidth))
+                let landedCoordinateY = skyView.bounds.height - (viewHeight / 2) - 16
+                
+                $0.center.x = landedCoordinateX
+                $0.center.y = landedCoordinateY
+                
+                birdsOnPlayground[$0]?.x = Float(landedCoordinateX - viewWidth / 2)
+                birdsOnPlayground[$0]?.y = Float(landedCoordinateY - viewHeight / 2)
+                birdsOnPlayground[$0]?.isFlying = false
+                
+                DatabaseManager.shared.updateCoreDataBird()
+            }
+        }
+    }
+    
     
     // MARK: -delegate pattern
     
     func updateInterface() {
+        skyView.subviews.forEach {
+            $0.removeFromSuperview()
+        }
         birdsList = DatabaseManager.shared.getCoreDataBirds()
+        addBirdsToPlayground()
     }
 }
