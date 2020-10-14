f0e17003f92c46e489598dd74c550eda479e7b24
Neestackich <neestackich@gmail.com>
2020-10-14 23:19:44 +0300

    triangle, label and text color added

--- Geese-Swans/Geese-Swans/Models/Managers/BirdsCreator.swift
+++ Geese-Swans/Geese-Swans/Models/Managers/BirdsCreator.swift
@@ -41,9 +41,25 @@ class BirdsCreator {
         newBird.backgroundColor = bird.color
         newBird.layer.cornerRadius = CGFloat(bird.size/6)
         
-        let nameLabel = UILabel(frame: CGRect(x: CGFloat(bird.x), y: CGFloat(bird.y), width: CGFloat(bird.size), height: CGFloat(bird.size/2)))
-        nameLabel.center = newBird.center
+        let nameLabel = UILabel(frame: CGRect(
+                                    x: newBird.bounds.minX,
+                                    y: newBird.bounds.minY,
+                                    width: CGFloat(bird.size),
+                                    height: CGFloat(bird.size)))
         nameLabel.text = bird.name
+        nameLabel.textAlignment = .center
+        nameLabel.font = UIFont(name: "Marker Felt", size: 16)
+        
+        switch bird.color {
+        case UIColor.black:
+            nameLabel.textColor = .systemGray3
+        case UIColor.gray, UIColor.darkGray:
+            nameLabel.textColor = .systemGray5
+        case UIColor.orange:
+            nameLabel.textColor = .systemGray5
+        default:
+            nameLabel.textColor = .systemGray2
+        }
         
         newBird.addSubview(nameLabel)
         
@@ -56,12 +72,39 @@ class BirdsCreator {
                                 y: CGFloat(bird.y),
                                 width: CGFloat(bird.size),
                                 height: CGFloat(bird.size)))
-        newBird.backgroundColor = bird.color
         newBird.layer.cornerRadius = CGFloat(bird.size/6)
         
-        let nameLabel = UILabel(frame: CGRect(x: CGFloat(bird.x), y: CGFloat(bird.y), width: CGFloat(bird.size), height: CGFloat(bird.size/2)))
-        nameLabel.center = newBird.center
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 0, y: Int(bird.size)))
+        path.addLine(to: CGPoint(x: Int(bird.size) / 2, y: 0))
+        path.addLine(to: CGPoint(x: Int(bird.size), y: Int(bird.size)))
+        path.addLine(to: CGPoint(x: 0, y: Int(bird.size)))
+
+        let shape = CAShapeLayer()
+        shape.path = path
+        shape.fillColor = bird.color?.cgColor
+        
+        newBird.layer.insertSublayer(shape, at: 0)
+        
+        let nameLabel = UILabel(frame: CGRect(
+                                    x: newBird.bounds.minX,
+                                    y: newBird.bounds.minY + newBird.bounds.height / 5,
+                                    width: CGFloat(bird.size),
+                                    height: CGFloat(bird.size)))
         nameLabel.text = bird.name
+        nameLabel.textAlignment = .center
+        nameLabel.font = UIFont(name: "Marker Felt", size: 16)
+        
+        switch bird.color {
+        case UIColor.black:
+            nameLabel.textColor = .systemGray3
+        case UIColor.gray, UIColor.darkGray:
+            nameLabel.textColor = .systemGray5
+        case UIColor.orange:
+            nameLabel.textColor = .systemGray5
+        default:
+            nameLabel.textColor = .systemGray2
+        }
         
         newBird.addSubview(nameLabel)
         
